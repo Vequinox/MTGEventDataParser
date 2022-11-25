cb5a0da8bb1e9987cbb15e1d81cbf73c52d85172
Vequinox <josephrogerahrens@gmail.com>
2022-11-25 13:27:08 -0600

    use waze api to calculate travel time instead of point to point distance

--- event_data.py
+++ event_data.py
@@ -1,12 +1,9 @@
 import datetime
-from uszipcode import SearchEngine
-import mpu
+import WazeRouteCalculator
+import logging
 from colorama import Fore
 from datetime import datetime
 
-engine = SearchEngine()
-
-WDM_ZIP = engine.by_zipcode('50265')
 YEAR = "2022"
 
 class Ev:
@@ -15,80 +12,71 @@ class Ev:
         self.store = store.strip()
         self.location = location.strip()
         self.frmt = frmt.strip()
-        self.dist = 0
+        self.travelHrs = 0
+        self.travelMins = 0
 
     def __lt__(self, other):
         return self.date < other.date
         
 def convertToDate(dateString):
-    splitDate = dateString.split(' ')
+    splitDate = dateString.split(" ")
     month = splitDate[0]
     day = splitDate[1]
 
-    return datetime.strptime(YEAR + '/' + month + '/' + day, '%Y/%b/%d').strftime('%Y/%m/%d')
-
-def calcDistance(zip1, zip2):
-    return round(mpu.haversine_distance((zip1.lat, zip1.lng), (zip2.lat, zip2.lng)) * 0.7, 2)
-
-def convertToMiles(steps):
-    return steps/(5280/3)
-
-def calcDrivingTime(zip1, zip2):
-    req = requests.get(f"http://router.project-osrm.org/route/v1/car/{zip1.lng},{zip1.lat};{zip2.lng},{zip2.lat}?overview=false""")
-    routes = json.loads(req.content)
-    route = routes.get("routes")[0]
-    distance = convertToMiles(route["distance"])
-    return distance
+    return datetime.strptime(YEAR + "/" + month + "/" + day, "%Y/%b/%d").strftime("%Y/%m/%d")
 
 def printColor(someEvent):
     color = Fore.WHITE
 
-    if(someEvent.dist >= 0):
+    if(someEvent.travelHrs >= 0):
         color = Fore.GREEN
-    if(someEvent.dist > 130):
+    if(someEvent.travelHrs >= 2):
         color = Fore.YELLOW
-    if(someEvent.dist > 200):
+    if(someEvent.travelHrs >= 3):
         color = Fore.RED
 
-    distance = "\tDistance: " + str(someEvent.dist)
+    duration = "\tDuration: " + str(someEvent.travelHrs) + " hrs " + str(someEvent.travelMins) + " mins"
     if len(someEvent.location) < 11:
-        distance = "\t" + distance
-    print(color + "Loc: " + someEvent.location + distance + " (" + someEvent.date + " " + someEvent.store + ", " + someEvent.frmt + ")")
+        duration = "\t" + duration
+    print(color + "Loc: " + someEvent.location + duration + " (" + someEvent.date + " " + someEvent.store + ", " + someEvent.frmt + ")")
 
 def parseData():
     allEvents = []
+    drivingFrom = input("Your address: ")
     showAll = input("Show all events? (y/s/n) ")
-    with open('events.txt') as data:
+
+    logger = logging.getLogger('WazeRouteCalculator.WazeRouteCalculator')
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    logger.addHandler(handler)
+
+    with open("events.txt") as data:
         lines = data.readlines()
-        numOfLines = len(lines)
-        lineLoop = 1
         for line in lines:
-            #print(line)
-            values = line.split('-')
-            evnt = Ev(values[0], values[1], values[2], values[3])
+            values = line.split("-")
 
-            c = evnt.location.split(',')[0]
-            s = evnt.location.split(',')[1].strip()
-            zipcodes = engine.by_city_and_state(city=c, state=s)
-            farthest = 0
-            for zip in zipcodes:
-                dist = calcDistance(WDM_ZIP, engine.by_zipcode(zip.zipcode))
-                if dist > farthest:
-                    farthest = dist
+            date = values[0]
+            store = values[1]
+            location = values[2]
+            mtgFormat = values[3]
+            evnt = Ev(date, store, location, mtgFormat)
+
+            drivingTo = store.strip() + ", " + location.strip()
+            route = WazeRouteCalculator.WazeRouteCalculator(drivingFrom, drivingTo, "US", avoid_toll_roads=True)
+            routeDuration = route.calc_route_info()[0]
+            evnt.travelHrs = int(routeDuration // 60)
+            evnt.travelMins = int(routeDuration % 60)
             
-            evnt.dist = farthest
-
-            if showAll == 'n':
-                if evnt.dist <= 200:
+            if showAll == "n":
+                if evnt.travelHrs <= 2:
                     allEvents.append(evnt)
-            elif showAll == 's':
-                if evnt.dist <= 400:
+            elif showAll == "s":
+                if evnt.travelHrs <= 3:
                     allEvents.append(evnt)
             else:
                 allEvents.append(evnt)
 
     for thisEvent in allEvents:
         printColor(thisEvent)
-
 
 parseData()
